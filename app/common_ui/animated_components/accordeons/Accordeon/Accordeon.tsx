9609b14e5c606c3cf525e4dd3bb67df89ff2bf56
dima <87605146+konata12@users.noreply.github.com>
2025-12-12 22:09:07 +0200

    created Accordeon

---
+++ app/common_ui/animated_components/accordeons/Accordeon/Accordeon.tsx
@@ -0,0 +1,25 @@
+import AnimatePresenceWithDynamicHeight from "@/app/common_ui/animated_components/AnimatePresenseWithDynamicHeight/AnimatePresenceWithDynamicHeight";
+import ArrowDropdown from "@/app/common_ui/buttons/ArrowBtn/ArrowDropdown";
+import React from "react";
+import styles from "./Accordeon.module.scss";
+
+interface AccordeonProps {
+	title: string;
+	children?: React.ReactNode;
+}
+
+export default function Accordeon({ title, children }: AccordeonProps) {
+	const [open, setOpen] = React.useState(false);
+
+	return (
+		<div className={styles.accordeon}>
+			<div className={styles.title_container}>
+				<h4 className={styles.title}>{title}</h4>
+				<ArrowDropdown directionTop={open} handleFunc={() => setOpen(!open)} />
+			</div>
+			<AnimatePresenceWithDynamicHeight childrenIsRendered={open}>
+				{children}
+			</AnimatePresenceWithDynamicHeight>
+		</div>
+	);
+}
